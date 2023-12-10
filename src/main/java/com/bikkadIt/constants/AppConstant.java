54c32c020ee32ee6e1855e15f47d04a78306268c
Rushikesh0909 <rushikeshhatkar@gmail.com>
2023-12-10 15:25:32 +0530

    product validation complited

--- src/main/java/com/bikkadIt/constants/AppConstant.java
+++ src/main/java/com/bikkadIt/constants/AppConstant.java
@@ -8,11 +8,13 @@ public class AppConstant {
 
     public static final String PAGE_NUMBER="0";
 
-    public static final String PAGE_SIZE="2";
+    public static final String PAGE_SIZE="5";
 
     public static final String SORT_BY="name";
 
     public static final String CATEGORY_SORT_BY="title";
     public static final String SORT_DIR="asc";
+
+    public static final String  PRODUCT_SORT_BY="title";
 
 }
--- src/main/java/com/bikkadIt/dto/ProductDto.java
+++ src/main/java/com/bikkadIt/dto/ProductDto.java
@@ -2,6 +2,9 @@ package com.bikkadIt.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Setter
@@ -11,16 +14,23 @@ import java.util.Date;
 @Builder
 public class ProductDto {
 
-    private String productId;
+    private  String productId;
 
+    @NotBlank
+    @Size(min = 5,max = 50,message = "Product Title Should Be Minimum 5 Character & Maximum 50 Character ")
     private String title;
 
+    @NotBlank
+    @Size(min = 5,max = 500,message = "Product  Description Should Be Minimum 5 Character & Maximum 500 Character ")
     private String description;
 
+    @NotNull(message = "Product Price Must Not Be Blank")
     private Double price;
 
+    @NotNull(message = "Product Price Must Not Be Blank")
     private Double discountedPrice;
 
+    @NotNull(message = "Quantity Should Not Be Blank")
     private Integer quantity;
 
     private Date addedDate;
